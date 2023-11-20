02a9946d9f1c6761114194ad6a567302995908fc
Edie Lemoine <edie@myparcel.nl>
2023-11-20 09:53:50 +0100

    feat: update outro

--- layouts/outro.vue
+++ layouts/outro.vue
@@ -1,12 +1,29 @@
 <template>
   <div
+    :class="{
+      'gap-4 grid-cols-3': image,
+    }"
     :style="style"
-    class="outro slidev-layout">
-    <slot />
+    class="grid h-full outro slidev-layout w-full">
+    <div
+      :class="{
+        'col-span-2 default image-right': image,
+      }"
+      v-bind="$attrs">
+      <slot />
+    </div>
+
+    <div
+      v-if="image"
+      class="my-14">
+      <img
+        :class="imageClass"
+        :src="image" />
+    </div>
 
     <div
       v-if="$slots.footer"
-      class="slidev-footer">
+      class="col-span-full slidev-footer">
       <slot name="footer" />
     </div>
   </div>
@@ -17,6 +34,14 @@ import {computed} from 'vue';
 import {handleBackground} from '../utils/handleBackground.ts';
 
 const props = defineProps({
+  image: {
+    type: String,
+    default: null,
+  },
+  imageClass: {
+    type: String,
+    default: null,
+  },
   background: {
     type: String,
     default: '/public/slide-bg.jpeg',
@@ -24,4 +49,10 @@ const props = defineProps({
 });
 
 const style = computed(() => handleBackground(props.background, true));
+</script>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
 </script>
